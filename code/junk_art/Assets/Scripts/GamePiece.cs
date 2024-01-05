5c44cc4f3485637fb42afba89b9fce32d5b21327
tomlnagel <32933566+tomlnagel@users.noreply.github.com>
2024-01-05 17:36:27 +0000

    alpha 0.1: refactoring interaction code

---
+++ code/junk_art/Assets/Scripts/GamePiece.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Properties, satuses and methods for game pieces
+*/
+public class GamePiece : MonoBehaviour
+{
+    [Header("Statuses")]
+    [SerializeField] public bool stacked;
+    [SerializeField] public bool grounded;
+    [SerializeField] public bool held;
+
+    //Materials
+    private Material baseMat; //default material for this piece
+    //private Material stackedMat; //material for a stacked piece
+    //private Material groundedMat; //material for a fallen peice
+    private Material heldMat; //material for a held peice
+    private MeshRenderer thisRenderer; //the object's renderer
+
+    //object destruction
+    private bool destroy;
+    private float detroyLen; //total seconds to flash before detruction
+    private float flashLen; //seconds per flash
+    private float destroyTimer; //seconds since flashing started
+    private float flashTimer; //seconds since last flash
+
+    //initialise piece
+    void Start()
+    {
+        baseMat = Resources.Load<Material>("Materials/New_Piece_Mat");
+        //stackedMat = Resources.Load<Material>("Materials/Stacked_Piece_Mat");
+        //groundedMat = Resources.Load<Material>("Materials/Grounded_Piece_Mat");
+        heldMat = Resources.Load<Material>("Materials/Held_Piece_Mat");
+
+        stacked = false;
+        grounded = false;
+        held = false;
+        thisRenderer = GetComponent<MeshRenderer>(); //get the renderer for this object
+
+        destroy = false;
+        detroyLen = 2.6f;
+        flashLen = 0.2f;
+        destroyTimer = 0f;
+        flashTimer = 0f;
+    }
+
+
+    void Update()
+    {
+        if (destroy)
+        {
+            FlashAndDestroy();
+        }
+    }
+
+    private void FlashAndDestroy()
+    {
+        destroyTimer += Time.deltaTime;
+        flashTimer += Time.deltaTime;
+
+        if (destroyTimer >= detroyLen)
+        {
+            Destroy(gameObject);
+        }
+        
+        if (flashTimer >= flashLen)
+        {
+            thisRenderer.enabled = !thisRenderer.enabled;
+            flashTimer = 0f;
+        }        
+    }
+
+    //get piece statuses
+    public string PrintStatuses()
+    {
+        string ret = "statuses for ";
+        ret += gameObject.name;
+        ret += "; stacked: ";
+        ret += stacked;
+        ret += " grounded: ";
+        ret += grounded;
+        ret += " held: ";
+        ret += held;
+        return ret;
+    }
+
+    //set piece status to 'held'
+    public void Pickup()
+    {
+        held = true;
+        thisRenderer.material = heldMat;
+    }
+
+    //unset held status
+    public void Drop()
+    {
+        held = false;
+        thisRenderer.material = baseMat;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (held)
+        {
+            //no collision triggers while piece held
+            return;
+        }
+
+        string otherTag = collision.gameObject.tag;
+        GamePiece otherPiece = collision.gameObject.GetComponent<GamePiece>();
+
+        //collision with 'ground'
+        if (otherTag == "Ground")
+        {
+            GroundPiece();
+            return;
+        }
+
+        //collsion with 'base'
+        if (otherTag == "Base")
+        {
+            AddToStack();
+            return;
+        }
+
+        //colided with something other than a game peice
+        if (!otherPiece)
+        {
+            return;
+        }
+
+        if (otherPiece.stacked && !otherPiece.grounded)
+        {
+            AddToStack();
+            return;
+        }
+    }
+
+    private void AddToStack()
+    {
+        //flag this piece as stacked
+        stacked = true;
+        //thisRenderer.material = stackedMat;
+    }
+
+    private void GroundPiece()
+    {
+        //flag this piece as grounded
+        grounded = true;
+        //thisRenderer.material = groundedMat;
+        destroy = true;
+    }
+}
---
+++ code/junk_art/Assets/Scripts/GrabObject.cs
@@ -0,0 +1,163 @@
+﻿#if ENABLE_INPUT_SYSTEM && ENABLE_INPUT_SYSTEM_PACKAGE
+#define USE_INPUT_SYSTEM
+    using UnityEngine.InputSystem;
+    using UnityEngine.InputSystem.Controls;
+#endif
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+
+    [Header("Grab settings")]
+    [SerializeField] private string grabTag = "GamePiece"; //tag of grabable object
+    [SerializeField] private Camera playerCamera; //current player's camera
+    [SerializeField] private Transform grabFocus; //grab focus point
+    [SerializeField] private Transform holdFocus; //hold focus point
+    [SerializeField] private float grabRange = 40f; //max range to grab object
+    [SerializeField] private float grabForce = 2f; //force to pull object to grab focus
+    [SerializeField] private float scrollSpeed = 0.5f; //speed to move while srolling
+    [SerializeField] private float maxScroll = 10f; //maximum Z offset from grab point
+    
+    private float scrollDistance = 0f; //current Z offset from grab point
+    private GameObject heldObject; //currently held object
+    private Rigidbody heldObjectRB; //RB of grabbed object
+    private bool rotating = false; //is the held object being rotated
+
+    //events
+    public delegate void StartRotating();
+    public static event StartRotating onRotateStart;
+
+    public delegate void StopRotating();
+    public static event StopRotating onRotateStop;
+
+    //once per frame
+    void Update()
+    {
+        //LMB pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            //check if already grabbing
+            if (heldObject)
+            {
+                dropObject();
+                return;
+            }
+
+            //cast ray through mouse location
+            Ray MouseRay = playerCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(MouseRay, out RaycastHit HitInfo, grabRange))
+            {
+                if (HitInfo.transform.gameObject.tag == grabTag)
+                {
+                    grabObject(HitInfo.transform.gameObject);
+                }                
+            }
+        }
+
+        if (heldObject)
+        {
+            //rotating mode
+            if (Input.GetKey(KeyCode.Space))
+            {
+                onRotateStart?.Invoke();
+                rotating = true;
+            }
+            else
+            {
+                onRotateStop?.Invoke();
+                rotating = false;
+
+                //restrict max scrolling
+                scrollDistance = Mathf.Clamp(scrollDistance + Input.mouseScrollDelta.y * scrollSpeed, -maxScroll, maxScroll);
+                //move hold position when scrolling
+                holdFocus.position = grabFocus.position + (holdFocus.TransformDirection(Vector3.forward) * scrollDistance);
+            }            
+        }
+    }
+
+    //once per physics timestamp
+    private void FixedUpdate()
+    {
+        if (heldObject)
+        {
+            moveObject();
+
+            if (rotating)
+            {
+                rotateObject();
+            }
+        }
+    }
+
+    private void grabObject(GameObject target)
+    {
+        //check target has an RB
+        if (target.GetComponent<Rigidbody>())
+        {
+            //assign grabbed object
+            heldObject = target;
+            heldObjectRB = target.GetComponent<Rigidbody>();
+
+            //set physics parameters
+            heldObjectRB.useGravity = false;
+            heldObjectRB.drag = 10;
+            heldObjectRB.constraints = RigidbodyConstraints.FreezeRotation;
+
+            //parent held object to grab area
+            heldObjectRB.transform.parent = holdFocus;
+
+            //set the object's status
+            heldObject.GetComponent<GamePiece>().Pickup();
+        }    
+    }
+
+    private void dropObject()
+    {        
+        //set physics parameters
+        heldObjectRB.useGravity = true;
+        heldObjectRB.drag = 1;
+        heldObjectRB.constraints = RigidbodyConstraints.None;
+
+        //reset status
+        heldObject.GetComponent<GamePiece>().Drop();
+
+        //unparent object
+        heldObjectRB.transform.parent = null;
+
+        //unassign grabbed object
+        heldObject = null;
+        heldObjectRB = null;
+
+        //reset hold focus to grab point
+        holdFocus.position = grabFocus.position;
+        scrollDistance = 0f;        
+    }
+
+    private void moveObject()
+    {
+        if (Vector3.Distance(heldObject.transform.position, holdFocus.position) > 0.1f)
+        {
+            //vector from object to hold point
+            Vector3 moveDirection = holdFocus.position - heldObject.transform.position;
+
+            //move using velocity
+            heldObjectRB.velocity = moveDirection * moveDirection.magnitude * grabForce;
+        }
+    }
+
+    private void rotateObject()
+    {
+        //get mouse movement
+        Vector3 mouseMovement = new Vector3(
+            Input.GetAxis("Mouse Y") * -1, 
+            Input.GetAxis("Mouse X") * -1, 
+            Input.mouseScrollDelta.y * 5
+            );
+
+        heldObjectRB.transform.eulerAngles += mouseMovement;
+    }
+
+}
